268c26a330fd47253d722dfb2dd1e4bb65e6ccc0
Демид Крисов <krisov.demid@gmail.com>
2024-11-10 17:50:29 +0300

    kz lang added

---
+++ RightVisionBotDb/Data/AcademyDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RightVisionBotDb.Data
+{
+    internal class AcademyDbContext : DbContext
+    {
+
+    }
+}
--- RightVisionBotDb/Models/CriticForm.cs
+++ RightVisionBotDb/Models/CriticForm.cs
@@ -51,5 +51,6 @@ namespace RightVisionBotDb.Models
 		}
 
 		#endregion
+
 	}
 }
