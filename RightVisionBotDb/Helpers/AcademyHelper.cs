96df09a1ad33072df580aa1109f89d2b5b7d46ae
Демид Крисов <krisov.demid@gmail.com>
2024-11-26 03:52:54 +0300

    Academy InDev

---
+++ RightVisionBotDb/Helpers/AcademyHelper.cs
@@ -0,0 +1,13 @@
+﻿using RightVisionBotDb.Types;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace RightVisionBotDb.Helpers
+{
+    public static class AcademyHelper
+    {
+        public static(string content, InlineKeyboardMarkup? keyboard) MainMenu(CallbackContext c)
+        {
+            return (string.Empty, null);
+        }
+    }
+}
